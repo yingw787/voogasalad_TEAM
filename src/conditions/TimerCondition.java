ff10f5d7cf161e96f9f863822baba8d97703fce8
William Yang <yangzwilliam@gmail.com>
2015-11-13 04:29:31 -0500

    forgot comments

--- src/conditions/TimerCondition.java
+++ src/conditions/TimerCondition.java
@@ -11,6 +11,7 @@ public class TimerCondition implements ICondition{
 	
 	/**
 	 * TODO
+	 * I'm not sure if this is how timeline works, but....
 	 * Make instance variable for storing a reference to the timeline
 	 * Make set method to set the timeline instance variable
 	 * Fill in checkCondition so that it checks if current time % myInterval == 0     <= this kinda works because myInterval will generally be very small
--- src/rules/Rule.java
+++ src/rules/Rule.java
@@ -16,7 +16,7 @@ public class Rule {
 	// Call this method with the containing unit as the parameter
 	// For the CheckAttribute condition, run this method in the event handler for any changed attribute action
 	// For the Collision condition, run this method in the event handler for intersects
-	// For the Time condition, 
+	// For the Time condition, check the TimerCondition class for instructions
 	public void run(Unit actor){
 		if(myCondition.checkCondition(actor)){
 			myAction.act(actor);
