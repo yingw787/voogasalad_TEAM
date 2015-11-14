c6f6a917da3a7b3fbf17be4dd0dcda0b9c7a21d5
William Yang <yangzwilliam@gmail.com>
2015-11-14 16:11:24 -0500

    put string attributes into map, added default constructors

--- src/units/Unit.java
+++ src/units/Unit.java
@@ -14,6 +14,7 @@ public class Unit {
 	protected int myBuyCost;
 	protected int mySellCost;
 	protected Map<String, Double> myAttributes;
+	protected Map<String, String> myStringAttributes;
 	
 	public Unit(String name, double health, double cd, String img, 
 			Point p, int ID, int bc, int sc){
@@ -32,14 +33,32 @@ public class Unit {
 		myAttributes.put("ID", (double)ID);
 		myAttributes.put("BuyCost", (double)bc);
 		myAttributes.put("SellCost", (double)sc);
+		myAttributes.put("X", (double)p.getX());
+		myAttributes.put("Y", (double)p.getY());
+		myStringAttributes = new HashMap<String, String>();
+		myStringAttributes.put("Name", name);
+		myStringAttributes.put("Image", img);
+	}
+	
+	public Unit(){
+		myAttributes = new HashMap<String, Double>();
+		myStringAttributes = new HashMap<String, String>();
 	}
 	
 	public double getAttribute(String attribute){
 		return myAttributes.get(attribute);
 	}
 	
+	public String getStringAttribute(String attribute){
+		return myStringAttributes.get(attribute);
+	}
+	
 	public void setAttribute(String attribute, double value){
 		myAttributes.put(attribute, value);
+	}
+	
+	public void setAttribute(String attribute, String value){
+		myStringAttributes.put(attribute, value);
 	}
 	
 	public String getImage() {
