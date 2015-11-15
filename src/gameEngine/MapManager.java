9599a4a5c6f2ca95b3f6e13550a18f6925079774
Ying Wang <yingw787@gmail.com>
2015-11-15 18:42:00 -0500

    Initialized backend engine modules for HUD.java and Map.java

---
+++ src/gameEngine/MapManager.java
@@ -0,0 +1,14 @@
+package gameEngine;
+
+public class MapManager {
+
+	/*
+	 * MapManager.java is the backend engine module for Map.java, the GamePlayer module in the front-end. 
+	 * Responsibilities: (keep adding to this as responsibilities grow and diverge: 
+	 * 
+	 * TODO still (update as needed:) 
+	 * 
+	 * 
+	 */
+	
+}
