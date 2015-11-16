a0488fe492c2d8196f5f502e273e428d3d4b0996
Vanessa Wu <vsw3@duke.edu>
2015-11-16 15:23:05 -0500

    commit to pull

--- src/gamedata/xml/TestObjectHolder.java
+++ src/gamedata/xml/TestObjectHolder.java
@@ -18,10 +18,10 @@ public class TestObjectHolder {
 
 	public TestObjectHolder() {
 		
-		tower1 = new Tower("tower1", "Tower", 100.0, 5.0, "tower1.jpg", new Point(60.0, 80.0), 1, 200, 100);
-		tower2 = new Tower("tower2", "Tower", 200.0, 7.0, "tower2.jpg", new Point(100.0, 135.0), 2, 400, 200);
-		troop1 = new Troop("troop1", "Troop", 2.0, 2.0, "troop1.jpg", new Point(75.0, 100.0), 1, 10, 2);
-		troop2 = new Troop("troop2", "Troop", 4.0, 3.0, "troop2.jpg", new Point(150.0, 35.0), 2, 20, 4);
+		tower1 = new Tower("tower1", 100.0, 5.0, "tower1.jpg", new Point(60.0, 80.0), 1, 200, 100);
+		tower2 = new Tower("tower2", 200.0, 7.0, "tower2.jpg", new Point(100.0, 135.0), 2, 400, 200);
+		troop1 = new Troop("troop1", 2.0, 2.0, "troop1.jpg", new Point(75.0, 100.0), 1, 10, 2);
+		troop2 = new Troop("troop2", 4.0, 3.0, "troop2.jpg", new Point(150.0, 35.0), 2, 20, 4);
 		
 		myTowers.add(tower1);
 		myTowers.add(tower2);
