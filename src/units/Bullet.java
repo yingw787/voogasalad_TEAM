53e332dff9ed930d926665bd858ee8b977aa4e7b
Vanessa Wu <vsw3@duke.edu>
2015-11-16 15:34:08 -0500

    fixed bullet constructor

--- src/units/Bullet.java
+++ src/units/Bullet.java
@@ -1,23 +1,11 @@
-//package units;
-//
-//<<<<<<< HEAD
-//public class Bullet extends Unit {
-//	
-//	private double myDamage;
-//	private int myHealth = 1;
-//	private double myX;
-//	private double myY;
-//	private Faction myFraction; 
-//=======
-//public class Bullet  extends Unit{
-//
-//>>>>>>> 925f685e169d98f5380bd1995a84cc31cd02c869
-//	
-//	public Bullet(String name,Faction faction, double cd, String img, 
-//			Point p){
-//		super(name,UnitType.Bullet,faction,1,cd,img,p,0,0);
-//	}
-//	
-//	//Insert method to check collision condition and apply damage
-//	
-//}
+package units;
+
+public class Bullet extends Unit{
+
+	public Bullet(String name, double health, double cd, String img, 
+			Point p, int ID, int bc, int sc){
+		super(name, health, cd, img, p, ID, bc, sc);
+	//Insert method to check collision condition and apply damage
+	
+	}
+}
