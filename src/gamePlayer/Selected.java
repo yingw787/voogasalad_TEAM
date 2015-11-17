303091f69bbd5e9aa6774bea6bfae065482e690d
denchgang <olimar64@gmail.com>
2015-11-16 19:12:20 -0500

    ability to "select unit" and display the parameters in HUD

---
+++ src/gamePlayer/Selected.java
@@ -0,0 +1,39 @@
+package gamePlayer;
+
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class Selected {
+
+	private View myView;
+	private VBox myVBox;
+	
+	private Text unit;
+	private Text health;
+	private Text sell;
+	
+	public Selected(View view){
+		this.myView = view;
+		myVBox = new VBox();
+		
+		unit = new Text("Unit: ");
+		health = new Text("Health: ");
+		sell = new Text ("Sell for: ");
+		
+		myVBox.getChildren().addAll(unit, health, sell);
+	}
+	
+	public void update(MapUnit myUnit){
+		unit.setText("Unit: " + myUnit.getUnit().getStringAttribute("Name"));
+		health.setText("Health: " + myUnit.getUnit().getAttribute("Health") 
+				+ "/" + myUnit.getUnit().getAttribute("MaxHealth"));
+		sell.setText("Sell for: " + myUnit.getUnit().getAttribute("SellCost"));
+	}
+	
+	public Node getDisplay(){
+		return myVBox;
+	}
+	
+}
