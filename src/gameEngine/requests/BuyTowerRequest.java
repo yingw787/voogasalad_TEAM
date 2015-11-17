c65ac9c0bc62cae683d28b912537ffbe4313dba0
Vanessa Wu <vsw3@duke.edu>
2015-11-16 23:16:44 -0500

    fixed buytower request

--- src/gameEngine/requests/BuyTowerRequest.java
+++ src/gameEngine/requests/BuyTowerRequest.java
@@ -1,15 +1,18 @@
 package gameEngine.requests;
 
 import gameEngine.environments.RuntimeEnvironment;
+import units.Point;
 import units.Tower;
 
 public class BuyTowerRequest extends Request {
 
 	private Tower myTower;
+	private Point myPoint;
 	
-	public BuyTowerRequest( Tower tower) {
+	public BuyTowerRequest(Tower tower, Point p) {
 		// TODO Auto-generated constructor stub
 		this.myTower = tower;
+		this.myPoint = p;
 	}
 
 	@Override
