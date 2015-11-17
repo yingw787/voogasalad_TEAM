736b254ba27087484b95b25e20b8a6da9dfb1a80
cw272 <cw272@duke.edu>
2015-11-16 21:16:29 -0500

    add request

---
+++ src/gameEngine/requests/SellTowerRequest.java
@@ -0,0 +1,21 @@
+package gameEngine.requests;
+
+import gameEngine.environments.RuntimeEnvironment;
+import units.Tower;
+
+public class SellTowerRequest extends Request {
+
+	Tower myTower;
+	
+	public SellTowerRequest(Tower tower) {
+		// TODO Auto-generated constructor stub
+		this.myTower = tower;
+	}
+
+	@Override
+	protected void execute(RuntimeEnvironment re) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
