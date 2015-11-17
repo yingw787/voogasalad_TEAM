9026edde871176bf1afc7d903f589db9612d25c8
Vanessa Wu <vsw3@duke.edu>
2015-11-17 10:05:22 -0500

    added progressbar to selecteddisplay:

--- src/gamePlayer/Selected.java
+++ src/gamePlayer/Selected.java
@@ -1,8 +1,7 @@
 package gamePlayer;
 
-import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
@@ -13,6 +12,7 @@ public class Selected {
 	
 	private Text unit;
 	private Text health;
+	private ProgressBar healthbar;
 	private Text sell;
 	
 	public Selected(View view){
@@ -21,15 +21,17 @@ public class Selected {
 		
 		unit = new Text("Unit: ");
 		health = new Text("Health: ");
+		healthbar = new ProgressBar(0);
 		sell = new Text ("Sell for: ");
 		unit.setStyle("-fx-font: 18px Tahoma;");
 		health.setStyle("-fx-font: 18px Tahoma;");
 		sell.setStyle("-fx-font: 18px Tahoma;");
-		myVBox.getChildren().addAll(unit, health, sell);
+		myVBox.getChildren().addAll(unit, health, healthbar, sell);
 	}
 	
 	public void update(MapUnit myUnit){
 		unit.setText("Unit: " + myUnit.getUnit().getStringAttribute("Name"));
+		healthbar.setProgress(myUnit.getUnit().getAttribute("Health")/myUnit.getUnit().getAttribute("MaxHealth"));
 		health.setText("Health: " + myUnit.getUnit().getAttribute("Health") 
 				+ "/" + myUnit.getUnit().getAttribute("MaxHealth"));
 		sell.setText("Sell for: " + (int) myUnit.getUnit().getAttribute("SellCost"));
