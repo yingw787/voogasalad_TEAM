fd6704f91815fd0433c983f9be0af69dba92c880
DENNIS XU <dx14@duke.edu>
2015-11-17 03:19:25 -0500

    xml methods take in account game title

--- src/gamedata/xml/TestObjectHolder.java
+++ src/gamedata/xml/TestObjectHolder.java
@@ -18,12 +18,16 @@ public class TestObjectHolder {
 	Troop troop2;
 	List<Unit> myTowers = new ArrayList<Unit>();
 	List<Unit> myTroops = new ArrayList<Unit>();
+	List<String> myGames = new ArrayList<String>();
 
 	public TestObjectHolder() {
-		tower1 = new Tower("tower1", 100.0, 5.0, "tower1.jpg", new Point(60.0, 80.0), 1, 200, 100);
-		tower2 = new Tower("tower2", 200.0, 7.0, "tower2.jpg", new Point(100.0, 135.0), 2, 400, 200);
-		troop1 = new Troop("troop1", 2.0, 2.0, "troop1.jpg", new Point(75.0, 100.0), 1, 10, 2);
-		troop2 = new Troop("troop2", 4.0, 3.0, "troop2.jpg", new Point(150.0, 35.0), 2, 20, 4);
+		tower1 = new Tower("tower1", 100.0, 5.0, "turret.png", new Point(60.0, 80.0), 1, 200, 100);
+		tower2 = new Tower("tower2", 200.0, 7.0, "turret_transparent.png", new Point(100.0, 135.0), 2, 400, 200);
+		troop1 = new Troop("troop1", 2.0, 2.0, "purpleminion.png", new Point(75.0, 100.0), 1, 10, 2);
+		troop2 = new Troop("troop2", 4.0, 3.0, "casterminion.png", new Point(150.0, 35.0), 2, 20, 4);
+		myGames.add("Game 1");
+		myGames.add("Game 2");
+		myGames.add("Game 3");
 		myTowers.add(tower1);
 		myTowers.add(tower2);
 		myTroops.add(troop1);
@@ -36,5 +40,9 @@ public class TestObjectHolder {
 	
 	public List<Unit> getTroops() {
 		return myTroops;
+	}
+	
+	public List<String> getGames() {
+		return myGames;
 	}
 }
--- src/gamedata/xml/TestXML.java
+++ src/gamedata/xml/TestXML.java
@@ -22,17 +22,18 @@ public class TestXML {
 	public void testTo() throws UnsupportedEncodingException, IOException {
 		XMLConverter c = new XMLConverter();
 		for (Unit tower : myTowers) {
-			c.toXML(tower, "Tower", tower.getStringAttribute("Name"));
+			c.toXML(tower, "Game 1", "Tower", tower.getStringAttribute("Name"));
 		}
 		for (Unit troop : myTroops) {
-			c.toXML(troop, "Troop", troop.getStringAttribute("Name"));
+			c.toXML(troop, "Game 1", "Troop", troop.getStringAttribute("Name"));
 		}
 	}
 	
+	@SuppressWarnings("unchecked")
 	public void testFrom() throws IOException {
 		XMLConverter c = new XMLConverter();
-		myTowersFromXML = c.fromXML("Tower");
-		myTroopsFromXML = c.fromXML("Troop");
+		myTowersFromXML = c.fromXML("Game 1", "Tower");
+		myTroopsFromXML = c.fromXML("Game 1", "Troop");
 		System.out.println(myTowersFromXML);
 		System.out.println(myTroopsFromXML);
 	}
