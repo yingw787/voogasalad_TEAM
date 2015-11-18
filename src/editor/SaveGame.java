7a33d3aba21cce46da5e3558b9e7f03282247784
DENNIS XU <dx14@duke.edu>
2015-11-18 16:24:56 -0500

    saving works

--- src/editor/SaveGame.java
+++ src/editor/SaveGame.java
@@ -1,16 +1,20 @@
 package editor;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import editor.tabData.DataController;
+import gamedata.xml.XMLConverter;
 import javafx.scene.control.Button;
 import units.Bullet;
 import units.Game;
 import units.Level;
 import units.Path;
+import units.PlayerInfo;
 import units.Tower;
 import units.Troop;
+import units.Unit;
 
 public class SaveGame {
 
@@ -38,6 +42,49 @@ public class SaveGame {
 		System.out.println(myPaths);
 		System.out.println(myLevels);
 		System.out.println(myGame);
+		
+		XMLConverter c = new XMLConverter();
+		String gameTitle = ((Game) myGame.get(0)).getTitle();
+		for (Object tower : myTowers) {
+			try {
+				c.toXML(tower, gameTitle, "Tower", ((Tower) tower).getStringAttribute("Name"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		for (Object troop : myTroops) {
+			try {
+				c.toXML(troop, gameTitle, "Troop", ((Troop)troop).getStringAttribute("Name"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		for (Object level : myLevels) {
+			try {
+				c.toXML(level, gameTitle, "Level", ((Level)level).getName());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		for (Object game : myGame) {
+			try {
+				c.toXML(game, gameTitle, "Game", gameTitle);
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		for (Object path : myPaths) {
+			try {
+				c.toXML(path, gameTitle, "Path", ((Path) path).getName());
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	public Button getSaveButton() {
