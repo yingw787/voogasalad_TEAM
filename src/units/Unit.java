cfae5e7bd7e3b2e334e9b13844e1d1ab1fe718a7
Wanning930 <jwnlinda930@hotmail.com>
2015-11-19 15:22:05 -0500

    before present

--- src/units/Unit.java
+++ src/units/Unit.java
@@ -7,8 +7,6 @@ import java.util.Set;
 
 import rules.Rule;
 
-import rules.Rule;
-
 public class Unit {
 	protected Map<String, Double> myAttributes;
 	protected Map<String, String> myStringAttributes;
