71b7ef43f9b5595a831956bc99d44451f5e1d441
Susan Lang <sml59@duke.edu>
2015-11-19 23:22:42 -0500

    Toolbar

--- src/startup/Startup.java
+++ src/startup/Startup.java
@@ -8,6 +8,7 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ToolBar;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -15,6 +16,14 @@ import javafx.stage.Stage;
 
 public class Startup {
 	private Stage myStage;
+	private double startSceneWidth = 400;
+	private double startSceneHeight = 200;
+	private double editorSceneWidth = 400;
+	private double editorSceneHeight = 300;
+	private double playerSceneWidth = 400;
+	private double playerSceneHeight = 300;
+	
+	// i know there is a lot of duplicated code right now but i will refactor later!!! - susan
 	
 	public Startup (Stage s) {
 		this.myStage = s;
@@ -43,22 +52,25 @@ public class Startup {
 
 		startVBox.getChildren().addAll(editorButton, playButton);
 
-		return new Scene(startPane, 400, 400);
+		return new Scene(startPane, startSceneWidth, startSceneHeight);
 	}
 	
 	private Scene createEditorScene() {
 		GridPane editorPane = new GridPane();
-		editorPane.setAlignment(Pos.CENTER);
-		VBox editorVBox= new VBox();
-		editorVBox.setSpacing(20.0);
-		editorPane.getChildren().add(editorVBox);
-		
+
 		Button backButton = new Button("Back");
-		backButton.setMaxWidth(Double.MAX_VALUE);
 		backButton.setOnAction(e-> {
 			myStage.setScene(createStartScene());
 		});
-		editorVBox.getChildren().add(backButton);
+		ToolBar myToolBar = new ToolBar();
+		myToolBar.getItems().add(backButton);
+		myToolBar.prefWidthProperty().bind(myStage.widthProperty());
+		editorPane.add(myToolBar, 0, 0);
+
+		VBox editorVBox= new VBox();
+		editorVBox.setSpacing(20.0);
+		editorVBox.setAlignment(Pos.CENTER);
+		editorPane.add(editorVBox, 0, 1);
 		
 		Button createButton = new Button("Create New Game");
 		createButton.setOnAction(e -> {
@@ -67,22 +79,25 @@ public class Startup {
 		});
 		editorVBox.getChildren().add(createButton);
 
-		return new Scene(editorPane, 400, 400);
+		return new Scene(editorPane, editorSceneWidth, editorSceneHeight);
 	}
 	
 	private Scene createPlayerScene() {
 		GridPane playerPane = new GridPane();
-		playerPane.setAlignment(Pos.CENTER);
-		VBox playerVBox= new VBox();
-		playerVBox.setSpacing(20.0);
-		playerPane.getChildren().add(playerVBox);
-		
+
 		Button backButton = new Button("Back");
-		backButton.setMaxWidth(Double.MAX_VALUE);
 		backButton.setOnAction(e-> {
 			myStage.setScene(createStartScene());
 		});
-		playerVBox.getChildren().add(backButton);
+		ToolBar myToolBar = new ToolBar();
+		myToolBar.getItems().add(backButton);
+		myToolBar.prefWidthProperty().bind(myStage.widthProperty());
+		playerPane.add(myToolBar, 0, 0);
+
+		VBox playerVBox= new VBox();
+		playerVBox.setSpacing(20.0);
+		playerVBox.setAlignment(Pos.CENTER);
+		playerPane.add(playerVBox, 0, 1);
 		
 		ChoiceBox<String> cb = new ChoiceBox<String>();
 		cb.setValue("Choose a game to load");
@@ -93,7 +108,6 @@ public class Startup {
 		playerVBox.getChildren().add(cb);
 		
 		Button loadButton = new Button("Load Game");
-		loadButton.setMaxWidth(Double.MAX_VALUE);
 		loadButton.setOnAction(e-> {
 			myStage.close();
 			try {
@@ -104,6 +118,6 @@ public class Startup {
 		});
 		playerVBox.getChildren().add(loadButton);
 		
-		return new Scene(playerPane, 400, 400);
+		return new Scene(playerPane, playerSceneWidth, playerSceneHeight);
 	}
 }
