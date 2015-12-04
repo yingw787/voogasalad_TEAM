e8b7747821a24a933d2ecbe8a4d08e7424b28332
Abhishek Upadhyaya Ghimire <au15@duke.edu>
2015-12-03 23:40:09 -0500

    created webview for help page in player; need to pass in the value from xml file

--- src/gamePlayer/HelpMenu.java
+++ src/gamePlayer/HelpMenu.java
@@ -1,7 +1,13 @@
 package gamePlayer;
 
+import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
+import javafx.stage.Stage;
 
 public class HelpMenu extends Menu{
 
@@ -17,6 +23,32 @@ public class HelpMenu extends Menu{
 	}
 
 	private void loadHelpPage() {
-		System.out.println("help");
+		Group rootMain = new Group();
+		Stage stage = new Stage();
+		stage.setTitle("Help Page");
+		Scene scene = new Scene(new Group());
+
+		stage.setWidth(1200);
+		stage.setHeight(700);
+		stage.show();
+
+		WebView browser = new WebView();
+		String url = "http://www.cs.duke.edu/courses/fall15/compsci308/assign/04_voogasalad/part5.php"; //hard coded; need to change this
+
+		WebEngine webEngine = browser.getEngine();
+		webEngine.load(url); 
+
+		ScrollPane scrollPane = new ScrollPane();
+		scrollPane.setContent(browser);
+		scrollPane.setPrefSize(1200, 700);
+		scrollPane.setFitToHeight(true);
+		scrollPane.setFitToWidth(true);
+
+		rootMain.getChildren().add(scrollPane);
+		scene.setRoot(rootMain);
+
+		stage.setScene(scene);
+		stage.show();	
 	}
+
 }
