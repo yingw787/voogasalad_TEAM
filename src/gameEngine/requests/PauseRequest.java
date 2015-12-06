220e8748e705258dd016ec657dcf16ab867ffc35
cw272 <cw272@duke.edu>
2015-12-05 22:41:50 -0500

    add two request

---
+++ src/gameEngine/requests/PauseRequest.java
@@ -0,0 +1,18 @@
+package gameEngine.requests;
+
+import controller.Controller;
+import gameEngine.environments.RuntimeEnvironment;
+
+public class PauseRequest extends Request {
+
+	public PauseRequest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute(RuntimeEnvironment re, Controller myController) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/gameEngine/requests/TwiceSpeedRequest.java
@@ -0,0 +1,18 @@
+package gameEngine.requests;
+
+import controller.Controller;
+import gameEngine.environments.RuntimeEnvironment;
+
+public class TwiceSpeedRequest extends Request {
+
+	public TwiceSpeedRequest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void execute(RuntimeEnvironment re, Controller myController) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
