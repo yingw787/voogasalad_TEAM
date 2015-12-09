56ffb2a1852c8d4928afe3569dae0cfad1c40ea0
William Yang (wzy) <yangzwilliam@gmail.com>
2015-12-08 21:36:03 -0500

    changed milliseconds to frames

--- src/rules/TimerDialog.java
+++ src/rules/TimerDialog.java
@@ -12,12 +12,12 @@ public class TimerDialog extends AConditionDialog {
 	public ICondition ask(Unit currentUnit) {
 		// Ask for delay
 		int delay = 0;
-		Optional<String> result = askUserForText("Timer Delay", "Enter a delay (ms) between actions", "Positive numbers only");
+		Optional<String> result = askUserForText("Timer Delay", "Enter a delay (frames) between actions", "Positive numbers only");
 		if (result.isPresent()){
 			delay = Integer.parseInt(result.get());
 		} else return null;
 		myCondition = new TimerCondition(delay);
-		myDescription = "Every " + delay + "milliseconds, ";
+		myDescription = "Every " + delay + " frames, ";
 		return myCondition;
 	}
 
