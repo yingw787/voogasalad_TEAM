db9a037963ee07a2c22bfca2dcc4ef47f77446e5
Wanning930 <jwnlinda930@hotmail.com>
2015-12-14 21:15:13 -0500

    code master piece

--- src/actions/StickAction.java
+++ src/actions/StickAction.java
@@ -1,3 +1,6 @@
+// This entire file is part of my masterpiece.
+// Wanning Jiang
+
 package actions;
 
 import java.util.Collection;
--- src/conditions/TimerCondition.java
+++ src/conditions/TimerCondition.java
@@ -1,3 +1,6 @@
+// This entire file is part of my masterpiece.
+// Wanning Jiang
+
 package conditions;
 
 import gameEngine.environments.RuntimeEnvironment;
